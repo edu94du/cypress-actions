854d0cc672683cd8b6086d7a94d5ab22989ef203
Eduardo <edu_jobsz@hotmail.com>
2024-05-13 10:08:35 -0300

    Update login.cy.js
    simulando um bug

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -29,6 +29,6 @@ describe('Login', () => {
     it('com sucesso', () => {
         cy.login('qa', 'xperience')
         cy.get(modalContent)
-            .should('have.text', 'Suas credenciais estão corretas :)')
+            .should('have.text', 'Suas credenciais estão ok :)')
     })
 })
